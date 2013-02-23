7f74bb1eb85ed27ab90c9b606014a7fd99c6cf18
vemacs <gentoo@outlook.com>
2013-02-23 07:54:25 -0700

    If I try hard enough

--- src/main/java/com/nullblock/vemacs/loadcontrol/LoadControl.java
+++ src/main/java/com/nullblock/vemacs/loadcontrol/LoadControl.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.event.world.WorldUnloadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoadControl extends JavaPlugin implements Listener {
@@ -25,27 +27,18 @@ public class LoadControl extends JavaPlugin implements Listener {
     public void onDisable() {
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onWorldInit(WorldInitEvent event) {
     	World world = event.getWorld();
     	String worldname = world.getName();
-    	List<String> worlds = this.getConfig().getStringList("worlds");
-    			if (!worlds.contains(worldname)) {
-    				world.setKeepSpawnInMemory(false);
-    				getLogger().info("Prevented " + worldname + "from keeping spawn loaded");
-    			}
-    	}
-
-    @EventHandler
-    public void onWorldLoad(WorldLoadEvent event) {
-    	World world = event.getWorld();
-    	String worldname = world.getName();
-    	List<String> worlds = this.getConfig().getStringList("worlds");
     	if (!this.parsedworlds.contains(worldname)) {
-    			if (!worlds.contains(worldname)) {
+    	List<String> worlds = this.getConfig().getStringList("worlds");
+    			if ( worldname.contains("myst") || worldname.contains("age") ) {
+    				world.setKeepSpawnInMemory(false);
     				Bukkit.unloadWorld(worldname, false);
     				this.parsedworlds.add(worldname);
-    				getLogger().info("Prevented " + worldname + "from loading");
+    				new WorldUnloadEvent(world);
+    				getLogger().info("Prevented " + worldname + " from keeping spawn loaded");
     			}
     	}
     }
